79df57574548e403794e76f0da90f008b1b1eae1
Lucas Rafael <lucasrafaelalves@yahoo.com.br>
2023-12-07 09:45:00 -0300

    subindo

--- frontend/medical_clinic/src/components/Home.vue
+++ frontend/medical_clinic/src/components/Home.vue
@@ -1,5 +1,48 @@
 <template>
-    <h3>Hello</h3>
+    <div class="root">
+    <div class="conteudo-ImagemPrincipal">
+        <div class="filtro-ImagemPrincipal">
+            <div class="Texto-Primario">
+                <h1 class="TextoBranco" style="margin-top: 40px;">AGENDAMENTO DE CONSULTAS MÉDICAS ESPECIALIZADAS DE FORMA FÁCIL, PRÁTICA E SEGURA.</h1>
+                <p class="TextoBranco" style="margin-top: 40px;">Mais um texto bonitinho contendo a descrição da clinica...</p>
+            </div>
+
+            <div class="Lista-CardDoutores">
+                <div class="CardDoutores">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="149" height="140" viewBox="0 0 149 140" fill="none" style="margin-top: 20px;" >
+                    <g style="mix-blend-mode:overlay;">
+                    <path d="M74.5 70C67.6708 70 61.8247 67.7153 56.9615 63.1458C52.0983 58.5764 49.6667 53.0833 49.6667 46.6667C49.6667 40.25 52.0983 34.7569 56.9615 30.1875C61.8247 25.6181 67.6708 23.3333 74.5 23.3333C81.3292 23.3333 87.1754 25.6181 92.0385 30.1875C96.9017 34.7569 99.3333 40.25 99.3333 46.6667C99.3333 53.0833 96.9017 58.5764 92.0385 63.1458C87.1754 67.7153 81.3292 70 74.5 70ZM24.8333 116.667V100.333C24.8333 97.0278 25.7398 93.9906 27.5526 91.2217C29.3654 88.4528 31.7701 86.3372 34.7667 84.875C41.182 81.8611 47.7007 79.6017 54.3229 78.0967C60.9451 76.5917 67.6708 75.8372 74.5 75.8333C81.3292 75.8333 88.0549 76.5878 94.6771 78.0967C101.299 79.6056 107.818 81.865 114.233 84.875C117.234 86.3333 119.641 88.4489 121.454 91.2217C123.266 93.9944 124.171 97.0317 124.167 100.333V116.667H24.8333Z" fill="#1E1E1E" />
+                    </g>
+                    </svg>
+                    <h2 class="TextoBranco2">Dr. Samuel</h2>
+                </div>
+                <div class="CardDoutores">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="149" height="140" viewBox="0 0 149 140" fill="none" style="margin-top: 20px;">
+                    <g style="mix-blend-mode:overlay;">
+                    <path d="M74.5 70C67.6708 70 61.8247 67.7153 56.9615 63.1458C52.0983 58.5764 49.6667 53.0833 49.6667 46.6667C49.6667 40.25 52.0983 34.7569 56.9615 30.1875C61.8247 25.6181 67.6708 23.3333 74.5 23.3333C81.3292 23.3333 87.1754 25.6181 92.0385 30.1875C96.9017 34.7569 99.3333 40.25 99.3333 46.6667C99.3333 53.0833 96.9017 58.5764 92.0385 63.1458C87.1754 67.7153 81.3292 70 74.5 70ZM24.8333 116.667V100.333C24.8333 97.0278 25.7398 93.9906 27.5526 91.2217C29.3654 88.4528 31.7701 86.3372 34.7667 84.875C41.182 81.8611 47.7007 79.6017 54.3229 78.0967C60.9451 76.5917 67.6708 75.8372 74.5 75.8333C81.3292 75.8333 88.0549 76.5878 94.6771 78.0967C101.299 79.6056 107.818 81.865 114.233 84.875C117.234 86.3333 119.641 88.4489 121.454 91.2217C123.266 93.9944 124.171 97.0317 124.167 100.333V116.667H24.8333Z" fill="#1E1E1E" />
+                    </g>
+                    </svg>
+                    <h2 class="TextoBranco2">Dr. Samuel</h2>
+                </div>
+                <div class="CardDoutores">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="149" height="140" viewBox="0 0 149 140" fill="none" style="margin-top: 20px;">
+                    <g style="mix-blend-mode:overlay;">
+                    <path d="M74.5 70C67.6708 70 61.8247 67.7153 56.9615 63.1458C52.0983 58.5764 49.6667 53.0833 49.6667 46.6667C49.6667 40.25 52.0983 34.7569 56.9615 30.1875C61.8247 25.6181 67.6708 23.3333 74.5 23.3333C81.3292 23.3333 87.1754 25.6181 92.0385 30.1875C96.9017 34.7569 99.3333 40.25 99.3333 46.6667C99.3333 53.0833 96.9017 58.5764 92.0385 63.1458C87.1754 67.7153 81.3292 70 74.5 70ZM24.8333 116.667V100.333C24.8333 97.0278 25.7398 93.9906 27.5526 91.2217C29.3654 88.4528 31.7701 86.3372 34.7667 84.875C41.182 81.8611 47.7007 79.6017 54.3229 78.0967C60.9451 76.5917 67.6708 75.8372 74.5 75.8333C81.3292 75.8333 88.0549 76.5878 94.6771 78.0967C101.299 79.6056 107.818 81.865 114.233 84.875C117.234 86.3333 119.641 88.4489 121.454 91.2217C123.266 93.9944 124.171 97.0317 124.167 100.333V116.667H24.8333Z" fill="#1E1E1E" />
+                    </g>
+                    </svg>
+                    <h2 class="TextoBranco2">Dr. Samuel</h2>
+                </div>
+            </div>
+            
+        </div> 
+    </div >
+    <div style="width: 100%; height: 36px; opacity: 0.5;
+background: linear-gradient(180deg, #31AFB4 0%, rgba(49, 175, 180, 0.86) 18.75%, rgba(49, 175, 180, 0.84) 28.65%, rgba(49, 175, 180, 0.80) 30.73%, rgba(49, 175, 180, 0.00) 99.98%, rgba(49, 175, 180, 0.03) 99.99%);
+
+">
+
+    </div>
+</div>
 </template>
 
 <script>
@@ -9,3 +52,77 @@
     }
 
 </script>
+
+
+<style scoped>
+ @import url('https://fonts.googleapis.com/css?family=Fira+Sans:400,500,600,700,800');
+
+.root{
+    display: flex;
+  flex-direction: column;
+}
+ .conteudo-ImagemPrincipal{
+width: 100%;
+height: 960px;
+flex-shrink: 0;
+background: url(../assets/Familia1.jpg), lightgray 50% ;
+background-size: cover;
+background-repeat: no-repeat;
+background-position: center center;
+
+}
+.filtro-ImagemPrincipal{
+    opacity: 0.5;
+     width: 100%;
+    height: 960px;
+    background: #31AFB4;
+}
+.Texto-Primario{
+    width: 25%;
+  float: left; 
+  box-sizing: border-box;
+}
+.TextoBranco{
+    color: #FFF;
+
+font-family: Inter;
+font-size: 30px;
+font-style: bold;
+font-weight: 600;
+line-height: normal;
+}
+.Lista-CardDoutores{
+    margin-top: 20px;
+    margin-right: 20px;
+    float: right; 
+    box-sizing: border-box;
+
+}
+.CardDoutores{
+    text-align: center; 
+    
+    justify-content: center;
+    align-items: center;
+
+    margin-bottom: 110px;
+    width: 283px;
+height: 209px;
+border-radius: 40px;
+border: 1px solid #FFF;
+background: linear-gradient(147deg, #C4C4C4 3.65%, rgba(255, 255, 255, 0.40) 3.65%, rgba(255, 255, 255, 0.10) 85%);
+box-shadow: 0px 4px 24px -1px rgba(0, 0, 0, 0.20);
+backdrop-filter: blur(20px);
+}
+.TextoBranco2{
+    color: #FFF;
+margin-right: 70px;
+
+font-family: Montserrat;
+font-size: 24px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
+letter-spacing: 3.24px;
+mix-blend-mode: overlay;
+}
+</style>
